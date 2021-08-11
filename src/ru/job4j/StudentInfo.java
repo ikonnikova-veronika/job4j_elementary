ab7d68fbe37ff2506482410319c39439128cf425
va.ikonnikova <va.ikonnikova@cdek.ru>
2021-08-11 22:56:24 +0700

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Veronika Ikonnikova");
+        System.out.println("15.10.1997");
+    }
+}
