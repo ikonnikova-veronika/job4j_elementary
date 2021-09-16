3bfa03540bafe1fb02befa59674ea98388e51a02
va.ikonnikova <va.ikonnikova@cdek.ru>
2021-09-16 16:32:29 +0700

    6.4. Слово начинается с ... [#41585 #203626]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,8 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            result = word[i] == pref[i];
-            if (!result) {
+            if (word[i] != pref[i]) {
+                result = false;
                 break;
             }
         }
